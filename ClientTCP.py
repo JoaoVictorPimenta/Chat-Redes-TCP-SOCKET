432ffea070c497e5ca266a39508f1e33315dd5fc
João Pimenta <joaovictorfp30@gmail.com>
2022-10-02 20:21:10 -0300

    Add codes

---
+++ ClientTCP.py
@@ -0,0 +1,72 @@
+import socket
+import threading #importa modulo socket
+
+TCP_IP = '127.0.0.1' # endereço IP do servidor 
+TCP_PORTA = 42005      # porta disponibilizada pelo servidor
+TAMANHO_BUFFER = 1024
+
+username = input("Insira seu username: ")
+
+# Criação de socket TCP do cliente
+cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Conecta ao servidor em IP e porta especifica 
+cliente.connect((TCP_IP, TCP_PORTA))
+
+# MENSAGEM  = input("Digite sua mensagem: ")
+
+def receive_messages():
+    while True:
+        try:
+            MENSAGEM = cliente.recv(TAMANHO_BUFFER).decode("utf-8")
+
+            if MENSAGEM == "@username":
+                cliente.send(username.encode("utf-8"))
+            
+            elif MENSAGEM == "QUIT":
+                cliente.close 
+                break       
+            
+            else:
+                print(MENSAGEM)
+        except:
+            print("Ocorreu um erro")
+            cliente.close
+            break
+
+def write_messages():
+    while True:
+        MENSAGEM = f"{username}: {input('')}"
+        cliente.send(MENSAGEM.encode("utf-8"))
+
+receive_thread = threading.Thread(target=receive_messages)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write_messages)
+write_thread.start()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # envia mensagem para servidor 
+# cliente.send(MENSAGEM.encode('UTF-8'))
+
+# # recebe dados do servidor 
+# data, addr = cliente.recvfrom(1024)
+
+# # fecha conexão com servidor
+# cliente.close()
+
+# print ("received data:", data)
---
+++ ServerTCP.py
@@ -0,0 +1,84 @@
+import socket
+import threading #importa modulo socket
+ 
+TCP_IP = '127.0.0.1' # endereço IP do servidor 
+TCP_PORTA = 42005       # porta disponibilizada pelo servidor
+TAMANHO_BUFFER = 1024     # definição do tamanho do buffer
+ 
+# Criação de socket TCP
+# SOCK_STREAM, indica que será TCP.
+servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# IP e porta que o servidor deve aguardar a conexão
+servidor.bind((TCP_IP, TCP_PORTA))
+
+#Define o limite de conexões. 
+servidor.listen()
+
+print(f"Servidor {TCP_IP} dispoivel na porta {TCP_PORTA} e escutando.....") 
+
+clientes = []
+usernames = []
+
+def broadcast(MENSAGEM, _cliente):
+    for cliente in clientes:
+        if cliente != _cliente:
+            cliente.send(MENSAGEM)
+
+def handle_messages(cliente):
+    while True:
+        try:
+            MENSAGEM = cliente.recv(TAMANHO_BUFFER)
+            broadcast(MENSAGEM,cliente)
+            if MENSAGEM == "QUIT":
+                index = clientes.index(cliente)
+                clientes.remove(cliente)
+                cliente.close()
+                username = usernames[index]
+                broadcast(f"Chatbot: {username} disconnected".encode('utf-8'), cliente)
+                usernames.remove(username)
+                break
+
+        except:
+            index = clientes.index(cliente)
+            clientes.remove(cliente)
+            cliente.close()
+            username = usernames[index]
+            broadcast(f"Chatbot: {username} disconnected".encode('utf-8'), cliente)
+            
+            usernames.remove(username)
+            
+            break
+
+def receive_connections():
+    while True:
+        cliente, address = servidor.accept()
+        cliente.send("@username".encode("utf-8"))
+        username = cliente.recv(TAMANHO_BUFFER).decode("utf-8")
+        clientes.append(cliente)
+        usernames.append(username)
+        print(f"{username} is connected with {str(address)}")
+        MENSAGEM = f"ChatBot: {username} joined the chat!".encode("utf-8")
+        broadcast(MENSAGEM,cliente)
+        cliente.send("Connected to server".encode("utf-8"))
+        
+        thread = threading.Thread(target=handle_messages, args=(cliente,))
+        thread.start()
+
+receive_connections()
+
+
+
+
+
+
+# # Aceita conexão 
+# conn, addr = servidor.accept()
+# print ('Endereço conectado:', addr)
+# while 1:
+#     #dados retidados da mensagem recebida
+#     data = conn.recv(TAMANHO_BUFFER)
+#     if data: 
+#         print ("Mensagem recebida:", data)  
+#         conn.send(data.upper())  # envia dados recebidos em letra maiuscula 
+
